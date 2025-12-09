7c57e9f92cd582dc0b860ba86a206b5955e3a46e
HamzaRauf1947 <hamzaraufstudy@gmail.com>
2025-12-09 22:59:59 +0500

    solve reverse str using stack did 2 method

---
+++ 8)Reverse String using stack/reverseStr.js
@@ -0,0 +1,48 @@
+// const reverseStr = (str)=>{
+//     const stack =[];
+//    let newStr = ""
+
+
+//    for (let char of str) {
+//         stack.push(char);
+//    }
+
+
+// for (let char of str) {
+//     newStr += stack.pop();
+// }
+
+// return newStr
+
+
+
+
+// }
+
+// console.log(reverseStr("hello world"));
+
+
+
+
+
+
+
+
+
+//? Another Method
+
+
+// const reverseStr = (str)=>{
+//     let val = str.split("")
+//     let stack = []
+  
+//     while(val.length>0){
+//         stack.push(val.pop())
+//     }
+   
+//     return stack.join("")
+    
+   
+// }
+
+// console.log(reverseStr("Hello"));
