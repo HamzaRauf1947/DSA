d6a04ca630c801f716fb2d5c076c4a596731b377
hamzaraufstudy@gmail.com <HamzaRauf1947>
2025-12-19 19:39:29 +0500

    Initial commit after windows reinstall

---
+++ hash.js
@@ -0,0 +1,15 @@
+class HashTable{
+    constructor(size=5){
+        this.keyMap = new Array(size);
+    }
+
+    _hashFunction(key){
+        let sum = 0;
+        const PrimeNum = 31;
+
+        for (let i = 0; i < Math.min(key.length,100); i++) {
+            
+            
+        }
+    }
+}
